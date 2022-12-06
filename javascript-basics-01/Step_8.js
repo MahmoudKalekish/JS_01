4b8b9d5946814be7ea3709c91582d914a295f222
Mahmoud <mahmoudkalekish18@gmail.com>
2022-12-06 13:30:56 +0200

    Step_8

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,10 @@
+function result(){
+    var age =document.getElementById("age").value;
+    if(age >18) {
+return alert("you are over 18");
+    }
+    else {
+       return alert("you are under 18");
+    }
+}
+
