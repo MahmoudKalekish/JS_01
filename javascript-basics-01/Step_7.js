5cd10f76372181a476831a3850bd1b538df0ada2
Mahmoud <mahmoudkalekish18@gmail.com>
2022-12-06 13:12:25 +0200

    Step_7

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,5 @@
+function total(){
+    num1 = document.getElementById("shoe_size").value;
+    num2 = document.getElementById("year").value;
+document.getElementById("result").innerHTML = (((num1 *2 +5) * 50)-num2)+1766;
+}
