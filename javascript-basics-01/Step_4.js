c1e405a73531d178e9a2c7ae8114fe0ec69dec7e
Mahmoud <mahmoudkalekish18@gmail.com>
2022-12-07 22:00:10 +0200

    Step_4

--- javascript-basics-01/Step_4.js
+++ javascript-basics-01/Step_4.js
@@ -1,4 +1,6 @@
-var a = prompt("Please enter your name: ");
-var b = prompt("Please enter your surname: ");
-var c = prompt("Please enter your city: ");
-alert("Hello "+ a + "\nyour surname is: "+b+"\nand you are from: " +c);
+function myFunction(){
+    name=document.getElementById("name").value;
+    surname=document.getElementById("surname").value;
+    city=document.getElementById("city").value;
+    alert("Your name: "+ name+"\nSurname: "+surname +"\nYour city is: "+city);
+}
