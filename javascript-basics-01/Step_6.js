bb01cc64f804347496ee04c2040eaa306d162e41
Mahmoud <mahmoudkalekish18@gmail.com>
2022-12-07 22:06:24 +0200

    Step_6

--- javascript-basics-01/Step_6.js
+++ javascript-basics-01/Step_6.js
@@ -1,6 +1,7 @@
 function divideBy() 
 { 
-        num1 = document.getElementById("first_number").value;
-        num2 = document.getElementById("second_number").value;
-document.getElementById("result").innerHTML = num1 / num2;
+        x=document.getElementById("first_number").value;
+        y=document.getElementById("second_number").value;
+        var z=x/y;
+        alert("The result of " +x+"/"+y+"=" +z);
 }
