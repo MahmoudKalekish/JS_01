3fd1026b1ee7b414269114f99e3c4c3681033c27
Mahmoud <mahmoudkalekish18@gmail.com>
2022-12-08 14:00:14 +0200

    Step1

---
+++ javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,9 @@
+function showhide(use){
+    var texte = document.getElementById("texte");
+    if(use==="show"){
+        texte.style.display="block";
+    } else if(use==="hide"){
+        texte.style.display="none";
+    }
+
+}
---
+++ javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,9 @@
+function re() {
+    document.getElementById("text").style.color="red";
+}
+function gr() {
+    document.getElementById("text").style.color = "green";
+}
+function bl() {
+    document.getElementById("text").style.color = "blue";
+}
