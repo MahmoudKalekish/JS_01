b3cf9ede1b163ea610735eb3705d9232ab1ec94e
Mahmoud <mahmoudkalekish18@gmail.com>
2022-12-08 13:21:48 +0200

    Step_1

---
+++ javascript-basics-02/step 1/Step_1.js
@@ -0,0 +1,8 @@
+
+function mouseOver() {
+  document.querySelector("#image1").src="./images/image1_2.jpg";
+}
+
+function mouseOut() {
+  document.querySelector("#image1").src="./images/image1.jpg";
+}
